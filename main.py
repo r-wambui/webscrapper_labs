2af2a38f230d84aa455433e0b0f1ef3bf0a3396c
r-wambui <rose.wambui780@gmail.com>
2020-07-13 17:21:39 +0300

    Edit file

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ def search_tender(title):
         return {"error": "No such jobs available"}, 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
